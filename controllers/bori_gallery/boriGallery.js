35060a9d2f63b5aaba337b905a74c501557d94e7
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-02 17:55:02 +0900

    feat: 보리갤러리에 컨트롤러 구현

---
+++ controllers/bori_gallery/boriGallery.js
@@ -0,0 +1,118 @@
+const BoriGallery = require("../../schemas/bori_gallery/boriGallery");
+const User = require("../../schemas/user/user");
+
+exports.getBoriGallery = async (req, res, next) => {
+  try {
+    const boriGallery = await BoriGallery.find({});
+    return res.status(200).json(boriGallery);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+exports.getBoriGalleryById = async (req, res, next) => {
+  try {
+    const boriGallery = await BoriGallery.findById(
+      req.params.bori_gallery_id
+    );
+    return res.status(200).json(boriGallery);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+exports.makeBoriGallery = async (req, res, next) => {
+  const { bori_gallery } = req.body;
+  try {
+    const boriGallery = await BoriGallery.create({
+      user_id: req.body.user_id,
+      bori_gallery_title: bori_gallery.bori_gallery_title,
+      bori_gallery_desc: bori_gallery.bori_gallery_desc,
+      bori_gallery_img: `/img/${req.file.filename}`
+    });
+    return res.status(200).json(boriGallery);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+exports.updateBoriGallery = async (req, res, next) => {
+  const { bori_gallery } = req.body;
+  try {  
+    const boriGallery = await BoriGallery.findByIdAndUpdate(bori_gallery._id, {
+      bori_gallery_title: bori_gallery.bori_gallery_title,
+      bori_gallery_desc: bori_gallery.bori_gallery_desc
+    });
+    return res.status(200).json(boriGallery);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+exports.updateBoriGalleryImage = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.params.user_id, {
+      profile_image: `/img/${req.file.filename}`,
+    });
+    res.status.json(`/img/${req.file.filename}`);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+exports.deleteBoriGallery = async (req, res, next) => {
+  try {  
+    await BoriGallery.remove({_id: req.body.bori_gallery_id});
+    return res.status(200).send("정상적으로 삭제했습니다!");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+}
+
+exports.likeBoriGallery = async (req, res, next) => {
+  try {
+    await BoriGallery.findByIdAndUpdate(
+      req.body.bori_gallery_id, {
+        bori_gallery_like: { $inc: 1 }
+      }
+    );
+    const user = await User.findByIdAndUpdate(
+      req.body.user_id, {
+        $push: { user_bori_gallery_like: req.body.bori_gallery_id }
+      }
+    );
+    res.status(200).json({
+      user_bori_gallery_like: user.user_bori_gallery_like
+    });
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+exports.dislikeBoriGallery = async (req, res, next) => {
+  try {
+    await BoriGallery.findByIdAndUpdate(
+      req.body.bori_gallery_id, {
+        bori_gallery_like: { $inc: -1 }
+      }
+    );
+    const user = await User.findByIdAndUpdate(
+      req.body.user_id, {
+        $pull: { user_bori_gallery_like: req.body.bori_gallery_id }
+      }
+    );
+    res.status(200).json({
+      user_bori_gallery_like: user.user_bori_gallery_like
+    });
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
---
+++ controllers/bori_gallery/boriGalleryReply.js
@@ -0,0 +1,129 @@
+const BoriGalleryReply = require("../../schemas/boriGallery/boriGalleryReply")
+
+//보리갤러리에 대한 모든 댓글정보를 가지고 온다.
+exports.getBoriGalleryReply = async (req, res, next) => {
+  try {
+    const boriGalleryReply = await BoriGalleryReply.find({
+      bori_gallery_id: req.params.bori_gallery_id
+    });
+    return res.status(200).json(boriGalleryReply);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+
+//맨처음 댓글을 생성한 케이스.
+exports.makeBoriGalleryReply = async (req, res, next) => {
+  try {
+    const boriGalleryReply = await BoriGalleryReply.create({
+      user_id: req.body.user_id,
+      bori_gallery_id: req.body.bori_gallery_id,
+      content: req.body.content,
+      reply_child: []
+    });
+    return res.status(200).json(boriGalleryReply);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+
+/*
+  {
+    reply_id: ~~~,
+    user_id: ~~~,
+    content: ~~~
+  }
+*/
+exports.makeProductChildReply = async (req, res, next) => {
+  try {
+    await BoriGalleryReply.findByIdAndUpdate(req.body.reply_id, {
+      $push: {
+        user_id: req.body.user_id,
+        content: req.body.content,
+        created_at: new Date.now
+      }
+    });
+    return res.status(200).json("대댓글 등록 완료.");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+//댓글은 내용만 수정하므로 content값만 가지고 온다.
+/*
+  {
+    reply_id: ~~~,
+    content: ~~~
+  }
+*/
+exports.updateBoriGalleryReply = async (req, res, next) => {
+  try {
+    const boriGalleryReply = await BoriGalleryReply.findByIdAndUpdate(req.body.reply_id, {
+      content: req.body.content
+    });
+    return res.status(200).json(boriGalleryReply);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+//부모 댓글 아이디 정보와 자식 댓글 아이디정보를 가지고 온다.
+/*
+  {
+    reply_id: ~~~,
+    child_reply_id: ~~~,
+    content: ~~~
+  }
+*/
+exports.updateProductChildReply = async (req, res, next) => {
+  try {
+    await BoriGalleryReply.updateOne({ _id: req.body.reply_id, "reply_child._id": req.body.child_reply_id}, {
+      $set: { "reply_child.$.content": req.body.content }
+    });
+    return res.status(200).json("댓글 업데이트 완료.");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+/*
+  {
+    reply_id: ~~~
+  }
+*/
+exports.deleteBoriGalleryReply = async (req, res, next) => {
+  try {
+    await BoriGalleryReply.remove({
+      _id: req.body.reply_id
+    });
+    return res.status(200).json("댓글 삭제 완료.");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+/*
+  {
+    reply_id: ~~~,
+    child_reply_id: ~~~
+  }
+*/
+exports.deleteProductChildReply = async (req, res, next) => {
+  try {
+    await BoriGalleryReply.findByIdAndUpdate(req.body.reply_id, {
+      $pull: { user_like: req.body.child_reply_id }
+    });
+    return res.status(200).json("대댓글 삭제 완료.");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
