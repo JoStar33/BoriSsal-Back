518e6ecd7537c221909f04e1905fde0291619dd3
HoCG <rhwkd5012@naver.com>
2023-03-02 20:54:10 +0900

    feat: 필요없는 파일 삭제

--- routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getProduct, getProductById, makeProduct } = require("../controllers/product")
-const router = express.Router();
-
-router.get('/', getProduct);
-
-router.get('/:product_id', getProductById);
-
-router.post('/', makeProduct);
