7c651a1f73ec937a925800fad2ed656b66674e4f
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-02 16:10:56 +0900

    chore: 배송지 주소 수정기능 변경

--- controllers/user/deliverAddress.js
+++ controllers/user/deliverAddress.js
@@ -3,12 +3,11 @@ const DeliverAddress = require("../../schemas/user/deliverAddress");
 exports.modifyDeliverAddress = async (req, res, next) => {
   const { user_id, address, address_detail } = req.body;
   try {
-    const deliverAddress = await DeliverAddress.update({
-      user_id: user_id,
+    const deliverAddress = await DeliverAddress.findOneAndUpdate({
+      user_id: user_id
+    }, {
       address: address,
       address_detail: address_detail
-    }, {
-      nick: req.body.user.nick,
     });
     return res.status(200).json(deliverAddress);
   } catch(error) {
