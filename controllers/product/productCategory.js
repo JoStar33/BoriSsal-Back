356cc127af1da3ce77f53194da0b5443e0f6771b
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-02 12:45:06 +0900

    feat: 카테고리 구현

---
+++ controllers/product/productCategory.js
@@ -0,0 +1,39 @@
+const ProductCategory = require("../../schemas/product/productCategory")
+
+exports.makeCategory = async (req, res, next) => {
+  try {
+    const productCategory = await ProductCategory.create({
+      category_name: req.body.category_name
+    });
+    return res.status(200).json(productCategory);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  };
+};
+
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const productCategory = await ProductCategory.update({
+      _id: req.body.category_id
+    }, {
+      category_name: req.body.category_name
+    });
+    return res.status(200).json(productCategory);
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const productCategory = await ProductCategory.remove({
+      _id: req.body.category_id
+    });
+    return res.status(200).send("카테고리 삭제 완료");
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+};
