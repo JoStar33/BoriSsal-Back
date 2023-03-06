b0e7369296ae9c05ed3089d0d72962aac5d6c52a
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-06 11:28:17 +0900

    feat: 보리갤러리 댓글 라우터 구현

---
+++ routes/bori_gallery/boriGalleryReply.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { getBoriGalleryReply, makeBoriGalleryReply, makeBoriGalleryChildReply, 
+  updateBoriGalleryReply, updateBoriGalleryChildReply, deleteBoriGalleryReply, deleteBoriGalleryChildReply } = require("../../controllers/bori_gallery/boriGalleryReply")
+const router = express.Router();
+// GET /:bori_gallery_id
+router.get('/:bori_gallery_id', getBoriGalleryReply);
+
+/*
+POST /bori-gallery-reply
+  {
+    user_id: ~~~,
+    product_id: ~~~,
+    content: ~~~
+  }
+*/
+router.post('/', makeBoriGalleryReply);
+
+/*
+POST /bori-gallery-reply/child
+  {
+    reply_id: ~~~,
+    user_id: ~~~,
+    content: ~~~
+  }
+*/
+router.post('/child', makeBoriGalleryChildReply);
+
+/*
+PATCH /bori-gallery-reply
+  {
+    reply_id: ~~~,
+    content: ~~~
+  }
+*/
+router.patch('/', updateBoriGalleryReply);
+
+/*
+PATCH /bori-gallery-reply/child
+  {
+    reply_id: ~~~,
+    child_reply_id: ~~~,
+    content: ~~~
+  }
+*/
+router.patch('/child', updateBoriGalleryChildReply);
+
+/*
+DELETE /bori-gallery-reply
+  {
+    reply_id: ~~~
+  }
+*/
+router.delete('/', deleteBoriGalleryReply);
+
+/*
+DELETE /bori-gallery-reply/child
+  {
+    reply_id: ~~~,
+    child_reply_id: ~~~
+  }
+*/
+router.delete('/child', deleteBoriGalleryChildReply);
+
+
+module.exports = router;
