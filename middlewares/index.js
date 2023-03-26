0ae85a5e84e356854355cd55f06ae72a1d06d835
HoCG <rhwkd5012@naver.com>
2023-03-26 18:24:20 +0900

    feat: 로그인 상태에서 로그아웃전용 api 호출시 강제 세션 파괴 및 로그아웃 처리

--- middlewares/index.js
+++ middlewares/index.js
@@ -10,6 +10,8 @@ exports.isNotLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     next();
   } else {
+    req.logout();
+    req.session.destroy();
     res.status(403).send({message: '로그인한 상태입니다.'});
   }
 };
