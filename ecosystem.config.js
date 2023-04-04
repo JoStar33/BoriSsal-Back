93136a14083ed1017b884f139bd5bf0b85683a7a
HoCG <rhwkd5012@naver.com>
2023-04-04 19:15:13 +0900

    chore: env파일을 제대로 못읽는 버그 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,6 @@ module.exports = {
   apps : [{
     name: "app",
     script: "./app.js",
-    instances: "max",
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
+    instances: "max"
   }]
 }
