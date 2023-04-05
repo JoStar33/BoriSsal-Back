8d09fbee9ea01ace413ea4dbc56a77d357dc0aa9
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-05 11:52:15 +0900

    chore: sameSite 적용

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ const sessionOption = {
     expires: new Date(Date.now() + days * 24 * 60 * 60 * 1000),
     httpOnly: false,
     secure: false,
+    sameSite: 'none'
   },
   store: mongoStore.create({
     mongoUrl:
