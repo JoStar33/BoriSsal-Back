1544a9256391ceeb2d7188e1d9e6f4bf4d783380
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-05 10:37:55 +0900

    chore: cors 재설정

--- app.js
+++ app.js
@@ -55,7 +55,9 @@ app.use('/bori_gallery_images', express.static(path.join(__dirname, 'bori_galler
 app.use(express.json());
 //전체 허용의 경우는 origin: true를 주자.
 app.use(cors({
-  origin: "*",
+  origin: [
+    process.env.REDIRECT_URL
+  ],
   credentials: true
 }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
